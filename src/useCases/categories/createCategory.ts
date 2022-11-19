807e43acd1017f0653629501508f12f246e9773f
Pasquale Milone Netto <pasqualenetto@yahoo.com.br>
2022-11-19 12:34:33 -0300

    api rest part1

---
+++ src/useCases/categories/createCategory.ts
@@ -0,0 +1,28 @@
+import {Request, Response} from 'express';
+
+import { Category } from '../../app/models/Category';
+
+export async function createCategory(req: Request, res: Response){
+  try{
+    const {icon, name} = req.body;
+
+    if(!name){
+      return res.status(400).json({
+        error: 'Name is required!'
+      });
+    }
+
+    if(!icon){
+      return res.status(400).json({
+        error: 'Icon is required!'
+      });
+    }
+    const category = await Category.create({icon,name,});
+
+    res.json(category);
+  }catch(error){
+    console.log(error);
+    res.status(500);
+  }
+
+}
